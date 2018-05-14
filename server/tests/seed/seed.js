65ef2b391453664b0ee1b9e4ee84d5b712f3077d
unknown <cattuong.cloud@gmail.com>
2018-05-14 14:47:48 +0700

    edit mocha test time out

--- server/tests/seed/seed.js
+++ server/tests/seed/seed.js
@@ -42,7 +42,8 @@ const populateTodos = (done) => {
   }).then(() => done());
 };
 
-const populateUsers = (done) => {
+const populateUsers = function(done){
+  this.timeout(15000);
   User.remove({}).then(() => {
     var userOne = new User(users[0]).save();
     var userTwo = new User(users[1]).save();
